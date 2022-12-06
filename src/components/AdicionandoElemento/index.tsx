ebc6460cb7d54de81edfab8b7ac67ea7e7425706
Elias de Souza <elias99-souza@hotmail.com>
2022-12-06 10:07:02 -0300

    testandoAPI

--- src/components/AdicionandoElemento/index.tsx
+++ src/components/AdicionandoElemento/index.tsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import FormularioElemento from "./FormularioElemento";
 import Lista from "./Lista";
 
 export default function ElementoInicial() {
 
+    useEffect(() => {
+        axios.get('http://localhost:3000/pessoas/')
+            .then((res) => {
+                console.log(res)
+            })
+    })
+
     const [nomes, setNomes] = useState([{
         nome: ' ',
-        
+
     }])
     return (
         <div>
 
             <FormularioElemento setNomes={setNomes} />
             <Lista nomes={nomes} />
-            
+
 
 
         </div>
