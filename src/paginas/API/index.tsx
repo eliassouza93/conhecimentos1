b7fb4af4ced4bec158f9296f60e0ca3871ab8c0d
Elias de Souza <elias99-souza@hotmail.com>
2022-12-07 09:16:15 -0300

    APINaza

--- src/paginas/API/index.tsx
+++ src/paginas/API/index.tsx
@@ -1,46 +1,41 @@
 import axios from "axios"
 import { Fragment, useEffect, useState } from "react"
 
-interface IBGE {
-
-    nome: string
-    freq: number
+interface INaza{
+    title:string
 }
-
 export default function ConsuAPI() {
 
-    const [data, setData] = useState<IBGE[]>([])
+    const [data, setData] = useState<INaza[]>([])
     const [carregando, setCarregando] = useState(false)
 
     useEffect(() => {
         setCarregando(true)
         axios.get('  https://api.nasa.gov/techport/api/projects/17792?api_key=rMVBJFjParA97UWJmMKxhiOR2WwUhollUEcnB4WH')
             .then(resposta => {
-                console.log(resposta)
+                setData(resposta.data)
             }).catch((erro) => {
                 console.log(erro)
                 setCarregando(true)
             }).finally(() => {
                 setCarregando(false)
             })
-
     }, [])
-
     return (
         <div  >
             {carregando ? (<h1>Carregando</h1>) : (
                 <div>
                     {data.map((user, index) => (
-                        <div>
-                         
-                        </div>
+                        <Fragment key={index}>
+                            <h1> {user.title} </h1>
+
+
+                        </Fragment>
                     ))}
 
                 </div>
 
-
             )}
-
 
         </div>
     )
