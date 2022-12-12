1168c0f73ee88ee738ccad72e0bcc28a38905b9a
Elias de Souza <elias99-souza@hotmail.com>
2022-12-12 20:26:14 -0300

    react_router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 
- 
+
 import ConsuAPI from 'paginas/API';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
--- src/paginas/API/index.tsx
+++ src/paginas/API/index.tsx
@@ -4,23 +4,20 @@ import { useEffect, useState } from "react"
 
 export default function ConsuAPI() {
 
-    const [naza, setNaza] = useState<[]>([])
+ 
+    const [clicar, setClicar] = useState(0)
 
-    useEffect(() => {
-        axios.get(' ')
-            .then((res) => {
-                console.log(res)
-
-                
-            })
-    }, [])
-
-
+    setTimeout(() => {
+        setClicar(clicar + 1099999999000500)
+    }, 0);
+ 
     return (
         <div>
             <h1>API da Nazsa</h1>
 
+            <button >Click + </button>
 
+            <h2>O botão foi clicado {clicar} vezes! </h2>
 
         </div>
 
