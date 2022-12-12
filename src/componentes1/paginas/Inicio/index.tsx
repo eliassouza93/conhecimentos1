a331f036077fca6a313cd3f6ec79c7c2a25bbc77
Elias de Souza <elias99-souza@hotmail.com>
2022-12-12 17:31:19 -0300

    react_router

--- src/componentes1/paginas/Inicio/index.tsx
+++ src/componentes1/paginas/Inicio/index.tsx
@@ -3,17 +3,28 @@ import { useEffect, useState } from "react"
 
 export default function Inicio() {
     const [conta, setConta] = useState(0)
+    useEffect(() => {
+
+        console.log('página recarregada!')
+        for (let i = 0; i < 300; i++) {
+            console.log("Block statement execution no." + i);
+            localStorage.setItem('key', 'Nome: ' + conta * 2)
+
+
+        }
 
 
 
 
-    const valor = 'Olá, mundo!'
+    })
+
 
     return (
         <div>
             <h1>Inicio</h1>
+            <button onClick={() => setConta(conta + 1)}>Adicionar</button>
+            <h2> {conta} </h2>
 
-         
 
         </div>
     )
