2fa3d87cf6b443c58749e218a70f161f5619c97f
Elias de Souza <elias99-souza@hotmail.com>
2022-12-13 09:21:31 -0300

    react_performance

---
+++ src/aluroni/components/menu/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function MenuAlura() {
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ src/aluroni/pages/cardapio/buscador/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function BuscadorAlura() {
+    return (
+        <div>
+
+        </div>
+    )
+}
